bac146c843b3fdfb8dbfb442a5892599ad45fdcf
Joshua Banton <joshb@krossover.com>
2017-04-04 16:00:00 -0400

    Adds dispatch_job method to support parameterized batch jobs. (/v1/job/<ID>/dispatch) Adds nomad response details to exceptions.

--- nomad/api/exceptions.py
+++ nomad/api/exceptions.py
@@ -1,10 +1,16 @@
-
-
 class BaseNomadException(Exception):
-
     """General Error occurred when interacting with nomad API"""
+    def __init__(self, nomad_resp):
+        if nomad_resp is not None:
+            print("Nomad Status: {status}".format(
+                status=nomad_resp.status_code))
+            print("Nomad Response: {resp}".format(resp=nomad_resp.text))
 
 
 class URLNotFoundNomadException(Exception):
-
     """The requeted URL given does not exist"""
+    def __init__(self, nomad_resp):
+        if nomad_resp is not None:
+            print("Nomad Status: {status}".format(
+                status=nomad_resp.status_code))
+            print("Nomad Response: {resp}".format(resp=nomad_resp.text))
